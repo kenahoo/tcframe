58643fe5b85d2ea32be7d63310b8d33581e9469e
ken <kenahoo@gmail.com>
2002-04-29 09:56:37 +0000

    Use a pre-scanning pass to do feature selection.
    Specify Naive Bayes threshold explicitly.

--- corpora/try_reuters.pl
+++ corpora/try_reuters.pl
@@ -2,6 +2,7 @@
 
 use strict;
 
+use lib '/Users/ken/src/tcframe/src/AI-Categorizer/lib';
 use AI::Categorizer::KnowledgeSet;
 use AI::Categorizer::Categorizer::NaiveBayes;
 
@@ -18,12 +19,25 @@ if (0) {
   my $k = new AI::Categorizer::KnowledgeSet
     (
      name => $name,
-     stopwords => \@stopwords,
+     stopwords => { map {($_ => 1)} @stopwords },
+     features_kept => 0.1,
+     verbose => 1,
     );
   
+  $k->scan_features( directory => "$name/training" );
+
+  warn "saving to $name-save";
+  $k->save_state("$name-save") or die $!;
+}
+
+if (0) {
+  ### Use new features
+  warn "restoring from $name-save";
+  my $k = AI::Categorizer::KnowledgeSet->restore_state("$name-save");
+
   ### Read categories
   my $cats = read_cats("$name/cats.txt");
-
+  
   ### Read documents
   opendir TRAIN_DIR, "$name/training" or die $!;
   while (my $file = readdir TRAIN_DIR) {
@@ -40,71 +54,20 @@ if (0) {
 
   #warn "categories are ", map {" ".$_->name." "} $k->categories;
   #warn "documents are ", map {" ".$_->name." "} $k->documents;
-  #warn "saving to $name-save";
-  #$k->save_state("$name-save") or die $!;
-
-
-
-  ### Do feature selection
-  #warn "restoring from $name-save";
-  #my $k = AI::Categorizer::KnowledgeSet->restore_state("$name-save");
-
-  { 
-    local $k->{verbose} = 1;
-    $k->select_features(features_kept => 0.2);
-  }
-
-  warn "saving features";
-  my @f = keys %{ $k->features->as_hash };  
-  Storable::store(\@f, 'features');
-}
-
-if (0) {
-  ### Use new features
-  my $f = Storable::retrieve('features');
-
-  my $k = new AI::Categorizer::KnowledgeSet
-    (
-     name => $name,
-     use_features => { map {($_ => 1)} @$f },
-    );
-  
-  ### Read categories
-  my %cats;
-  open CATS, "$name/cats.txt" or die $!;
-  while (<CATS>) {
-    my ($doc, @cats) = split;
-    $cats{$doc} = [@cats];
-  }
-  close CATS;
-  
-  ### Read documents
-  opendir TRAIN_DIR, "$name/training" or die $!;
-  while (my $file = readdir TRAIN_DIR) {
-    next if $file =~ /^\./;
-    
-    print "$file: @{$cats{$file}}\n";
-    my $body = do {open my $fh, "$name/training/$file" or die $!; local $/; <$fh>};  
-    $k->make_document( name => $file,
-		       categories => $cats{$file},
-		       content => $body );
-#last if $::i++ > 500;
-  }
-  closedir TRAIN_DIR;
-
-  #warn "categories are ", map {" ".$_->name." "} $k->categories;
-  #warn "documents are ", map {" ".$_->name." "} $k->documents;
   warn "saving to $name-save2";
   $k->save_state("$name-save2") or die $!;
 }
 
-if (0) {
+if (1) {
   ### Train the categorizer
   warn "restoring from $name-save2";
   my $k = AI::Categorizer::KnowledgeSet->restore_state("$name-save2");
 
   warn "training categorizer";
-  my $nb = new AI::Categorizer::Categorizer::NaiveBayes();
+  my $nb = new AI::Categorizer::Categorizer::NaiveBayes
+    (
+     bayes_threshold => 0.2,
+    );
   $nb->train(knowledge => $k);
 
   warn "saving to $name-save3";
@@ -127,7 +90,7 @@ if (1) {
 					  name => $file,
 					  content => $body,
 					 );
-    my $h = $nb->categorize($d->features);
+    my $h = $nb->categorize($d);
     print " => @{[ $h->categories ]}\n";
 #last if $::i++ > 500;
   }
