8ce4164e43bba460c01deea09819dd71e8d07a48
ken <kenahoo@gmail.com>
2003-01-04 01:19:08 +0000

    First version that works

--- doc/AIED-DrMath/two_level.pl
+++ doc/AIED-DrMath/two_level.pl
@@ -1,25 +1,53 @@
 #!/usr/bin/perl
 
+use strict;
 use AI::Categorizer;
 use AI::Categorizer::Learner::NaiveBayes;
 use AI::Categorizer::Collection::Files;
+use Statistics::Contingency;
 
+# Load the categorizers from disk
 my $level_c = AI::Categorizer::Learner->restore_state('/tmp/drmath-NB-level');
 my $topic_c = AI::Categorizer::Learner->restore_state('/tmp/drmath-NB-topic');
 
+# Access the test collection
+my $cat_file = 'corpora/drmath-1.00/cats.txt';
 my $test_set = AI::Categorizer::Collection::Files->new
   (
    path => 'corpora/drmath-1.00/test',
-   category_file => 'corpora/drmath-1.00/cats.txt',
+   category_file => $cat_file,
   );
 
-my %valid_cats = (); # ... get list of valid categories ...
 
+# Get list of valid categories
+my %valid_cats;
+open my($fh), $cat_file or die "Can't open $cat_file: $!";
+while (<$fh>) {
+  my ($doc, @cats) = split;
+  @valid_cats{@cats} = ();
+}
 
+my $e = new Statistics::Contingency(categories => [keys %valid_cats]);
+
+# Categorize
 while (my $doc = $test_set->next) {
   my $level_h = $level_c->categorize($doc);
   my $topic_h = $topic_c->categorize($doc);
-  print STDERR '.';
 
-  # ... combine the hypotheses into a set of categories to assign
+  my @assigned;
+  foreach my $level ($level_h->categories) {
+    foreach my $topic ($topic_h->categories) {
+      my $cat = "$topic.$level";
+      #print "$cat\n";
+      push @assigned, $cat if exists $valid_cats{$cat};
+    }
+  }
+
+  $e->add_result(\@assigned, [map $_->name, $doc->categories]);
+  
+  print STDERR '.';
 }
+print STDERR "\n";
+
+# Show results
+print $e->stats_table;
